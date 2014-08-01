64ef99c97ffdd8a5a1892e32a519cd9e41f10f3c
Fernando Petrola <fernando.petrola@dragome.com>
2014-08-01 20:44:47 -0300

    list property binding bug fix

--- dragome-form-bindings/src/main/java/com/dragome/forms/bindings/builders/TemplateComponentBindingBuilder.java
+++ dragome-form-bindings/src/main/java/com/dragome/forms/bindings/builders/TemplateComponentBindingBuilder.java
@@ -42,200 +42,222 @@ import com.dragome.templates.interfaces.Template;
 
 public class TemplateComponentBindingBuilder<T, C extends VisualComponent>
 {
-	private C component;
-	private Class<? extends VisualComponent> componentType;
-	private T model;
-	private VisualPanel panel;
-	private FormBinder binder= new FormBinder();
-	private Template template;
+    private C component;
+    private Class<? extends VisualComponent> componentType;
+    private T model;
+    private VisualPanel panel;
+    private FormBinder binder= new FormBinder();
+    private Template template;
 
-	public TemplateComponentBindingBuilder(Template template, VisualPanel panel, T model, Class<C> componentType)
+    public TemplateComponentBindingBuilder(Template template, VisualPanel panel, T model, Class<C> componentType)
+    {
+	this.panel= panel;
+	this.model= model;
+	this.template= template;
+	if (componentType.equals(VisualPanel.class))
+	    this.componentType= VisualPanelImpl.class;
+	else if (componentType.equals(VisualTextField.class))
+	    this.componentType= VisualTextFieldImpl.class;
+	else if (componentType.equals(VisualButton.class))
+	    this.componentType= VisualButtonImpl.class;
+	else if (componentType.equals(VisualCheckbox.class))
+	    this.componentType= VisualCheckboxImpl.class;
+	else if (componentType.equals(VisualRadioButton.class))
+	    this.componentType= VisualRadioButtonImpl.class;
+	else if (componentType.equals(VisualLink.class))
+	    this.componentType= VisualLinkImpl.class;
+	else if (componentType.equals(VisualLabel.class))
+	    this.componentType= VisualLabelImpl.class;
+
+	component= (C) ServiceLocator.getInstance().getReflectionService().createClassInstance(this.componentType);
+	setupComponent();
+    }
+
+    private void setupComponent()
+    {
+	component.setName(template.getName());
+	if (component instanceof VisualPanel)
+	    ((VisualPanel) component).initLayout(new TemplateLayout(template));
+    }
+
+    public TemplateComponentBindingBuilder(Template template, VisualPanel panel, T model, C component)
+    {
+	this.template= template;
+	this.panel= panel;
+	this.model= model;
+	this.component= component;
+	setupComponent();
+    }
+
+    public ComponentBuilder<T> childBuilder()
+    {
+	build();
+	return new ComponentBuilder<T>((VisualPanel) component, model);
+    }
+
+    public <S> RepeaterBuilder<S, T> toListProperty(final Getter<T, List<S>> getter)
+    {
+	final ValueModelDelegator<T, List<S>> valueModelDelegator= new ValueModelDelegator<T, List<S>>(model);
+
+	List<S> list= getter.get(model);
+	
+	addListenerIfObservable(valueModelDelegator, list);
+	
+	NullMutableValueModel<List<S>> valueSource= new NullMutableValueModel<List<S>>()
 	{
-		this.panel= panel;
-		this.model= model;
-		this.template= template;
-		if (componentType.equals(VisualPanel.class))
-			this.componentType= VisualPanelImpl.class;
-		else if (componentType.equals(VisualTextField.class))
-			this.componentType= VisualTextFieldImpl.class;
-		else if (componentType.equals(VisualButton.class))
-			this.componentType= VisualButtonImpl.class;
-		else if (componentType.equals(VisualCheckbox.class))
-			this.componentType= VisualCheckboxImpl.class;
-		else if (componentType.equals(VisualRadioButton.class))
-			this.componentType= VisualRadioButtonImpl.class;
-		else if (componentType.equals(VisualLink.class))
-			this.componentType= VisualLinkImpl.class;
-		else if (componentType.equals(VisualLabel.class))
-			this.componentType= VisualLabelImpl.class;
+	    public List<S> getDelegatedValue()
+	    {
+		return getter.get(model);
+	    }
+	};
+	
+	valueModelDelegator.setValueSource(valueSource);	
+	
+	BindingSync.addCondition(valueModelDelegator);
+	return new RepeaterBuilder<S, T>(valueModelDelegator, template, model, panel);
+    }
 
-		component= (C) ServiceLocator.getInstance().getReflectionService().createClassInstance(this.componentType);
-		setupComponent();
-	}
-
-	private void setupComponent()
+    private <S> void addListenerIfObservable(final ValueModelDelegator<T, List<S>> valueModelDelegator, List<S> list)
+    {
+	if (list instanceof ObservableList)
 	{
-		component.setName(template.getName());
-		if (component instanceof VisualPanel)
-			((VisualPanel) component).initLayout(new TemplateLayout(template));
-	}
-
-	public TemplateComponentBindingBuilder(Template template, VisualPanel panel, T model, C component)
-	{
-		this.template= template;
-		this.panel= panel;
-		this.model= model;
-		this.component= component;
-		setupComponent();
-	}
-
-	public ComponentBuilder<T> childBuilder()
-	{
-		build();
-		return new ComponentBuilder<T>((VisualPanel) component, model);
-	}
-
-	public <S> RepeaterBuilder<S, T> toListProperty(Getter<T, List<S>> getter)
-	{
-		final List<S> list= getter.get(model);
-		return toList(list);
-	}
-
-	public <S> RepeaterBuilder<S, T> toList(final List<S> list)
-	{
-		final ValueModelDelegator<T, List<S>> valueModelDelegator= new ValueModelDelegator<T, List<S>>(model);
-		NullMutableValueModel<List<S>> valueSource= new NullMutableValueModel<List<S>>()
+	    ObservableList<S> observableList= (ObservableList<S>) list;
+	    observableList.setListChangeListener(new ListChangedListener()
+	    {
+		public void listChanged()
 		{
-			public List<S> getDelegatedValue()
-			{
-				if (list instanceof ObservableList)
-				{
-					ObservableList<S> listWrapper= (ObservableList<S>) list;
-					listWrapper.setListChangeListener(new ListChangedListener()
-					{
-						public void listChanged()
-						{
-							valueModelDelegator.fireValueChangeEvent();
-						}
-					});
-				}
-				return list;
-			}
-		};
-
-		valueModelDelegator.setValueSource(valueSource);
-
-		BindingSync.addCondition(valueModelDelegator);
-		return new RepeaterBuilder<S, T>(valueModelDelegator, template, model, panel);
+		    valueModelDelegator.fireValueChangeEvent();
+		}
+	    });
 	}
+    }
 
-	public TemplateComponentBindingBuilder<T, C> onDoubleClick(DoubleClickListener doubleClickPerformed)
-	{
-		component.addDoubleClickListener(doubleClickPerformed);
-		return this;
-	}
+    public <S> RepeaterBuilder<S, T> toList(final List<S> list)
+    {
+	final ValueModelDelegator<T, List<S>> valueModelDelegator= new ValueModelDelegator<T, List<S>>(model);
+	addListenerIfObservable(valueModelDelegator, list);
 
-	public TemplateComponentBindingBuilder<T, C> onClick(ClickListener clickListener)
+	NullMutableValueModel<List<S>> valueSource= new NullMutableValueModel<List<S>>()
 	{
-		component.addClickListener(clickListener);
-		return this;
-	}
+	    public List<S> getDelegatedValue()
+	    {
+		return list;
+	    }
+	};
 
-	public TemplateComponentBindingBuilder<T, C> onClick(final ActionExecutor actionExecutor)
+	valueModelDelegator.setValueSource(valueSource);
+
+	BindingSync.addCondition(valueModelDelegator);
+	return new RepeaterBuilder<S, T>(valueModelDelegator, template, model, panel);
+    }
+
+    public TemplateComponentBindingBuilder<T, C> onDoubleClick(DoubleClickListener doubleClickPerformed)
+    {
+	component.addDoubleClickListener(doubleClickPerformed);
+	return this;
+    }
+
+    public TemplateComponentBindingBuilder<T, C> onClick(ClickListener clickListener)
+    {
+	component.addClickListener(clickListener);
+	return this;
+    }
+
+    public TemplateComponentBindingBuilder<T, C> onClick(final ActionExecutor actionExecutor)
+    {
+	component.addClickListener(new ClickListener()
 	{
-		component.addClickListener(new ClickListener()
+	    public void clickPerformed(VisualComponent aVisualComponent)
+	    {
+		actionExecutor.execute();
+	    }
+	});
+	return this;
+    }
+
+    public VisualComponent component()
+    {
+	return component;
+    }
+
+    public TemplateComponentBindingBuilder<T, C> onKeyUp(final KeyUpListener keyUpListener, final int... codes)
+    {
+	component.addKeyListener(new KeyUpListener()
+	{
+	    public void keyupPerformed(VisualComponent visualComponent, int keyCode)
+	    {
+		for (int code : codes)
 		{
-			public void clickPerformed(VisualComponent aVisualComponent)
-			{
-				actionExecutor.execute();
-			}
-		});
-		return this;
-	}
+		    if (code == keyCode || code == 0)
+		    {
+			keyUpListener.keyupPerformed(visualComponent, keyCode);
+			break;
+		    }
+		}
+	    }
+	});
+	return this;
+    }
 
-	public VisualComponent component()
+    public TemplateComponentBindingBuilder<T, C> onBlur(BlurListener blurListener)
+    {
+	component.addListener(BlurListener.class, blurListener);
+	return this;
+    }
+
+    public <S> TemplateComponentBindingBuilder<T, C> toProperty(final Getter<T, S> getter, final Setter<T, S> setter)
+    {
+	ValueModelDelegator<T, S> valueModelDelegator= new ValueModelDelegator<T, S>(new NullMutableValueModel<S>()
 	{
-		return component;
-	}
+	    public S getDelegatedValue()
+	    {
+		return getter.get(model);
+	    }
 
-	public TemplateComponentBindingBuilder<T, C> onKeyUp(final KeyUpListener keyUpListener, final int... codes)
+	    public void setValue(S value)
+	    {
+		setter.set(model, value);
+	    }
+	}, model);
+
+	return with(valueModelDelegator);
+    }
+
+    public <S> TemplateComponentBindingBuilder<T, C> toProperty(String propertyName)
+    {
+	return with(new ValueModelDelegator<T, S>(new NullMutableValueModel<S>(), model));
+    }
+
+    private <S> TemplateComponentBindingBuilder<T, C> with(ValueModelDelegator<T, S> valueModelDelegator)
+    {
+	binder.bind(valueModelDelegator).to((HasValue<S>) component);
+	BindingSync.addCondition(valueModelDelegator);
+	return this;
+    }
+
+    public <S> TemplateComponentBindingBuilder<T, C> to(final ValueSource<S> valueSource)
+    {
+	ValueModelDelegator<T, S> valueModelDelegator= new ValueModelDelegator<T, S>(new NullMutableValueModel<S>()
 	{
-		component.addKeyListener(new KeyUpListener()
-		{
-			public void keyupPerformed(VisualComponent visualComponent, int keyCode)
-			{
-				for (int code : codes)
-				{
-					if (code == keyCode || code == 0)
-					{
-						keyUpListener.keyupPerformed(visualComponent, keyCode);
-						break;
-					}
-				}
-			}
-		});
-		return this;
-	}
+	    public S getDelegatedValue()
+	    {
+		return valueSource.getValue();
+	    }
+	}, model);
 
-	public TemplateComponentBindingBuilder<T, C> onBlur(BlurListener blurListener)
-	{
-		component.addListener(BlurListener.class, blurListener);
-		return this;
-	}
+	return with(valueModelDelegator);
+    }
 
-	public <S> TemplateComponentBindingBuilder<T, C> toProperty(final Getter<T, S> getter, final Setter<T, S> setter)
-	{
-		ValueModelDelegator<T, S> valueModelDelegator= new ValueModelDelegator<T, S>(new NullMutableValueModel<S>()
-		{
-			public S getDelegatedValue()
-			{
-				return getter.get(model);
-			}
+    public C build()
+    {
+	panel.addChild(component);
+	return component;
+    }
 
-			public void setValue(S value)
-			{
-				setter.set(model, value);
-			}
-		}, model);
-
-		return with(valueModelDelegator);
-	}
-
-	public <S> TemplateComponentBindingBuilder<T, C> toProperty(String propertyName)
-	{
-		return with(new ValueModelDelegator<T, S>(new NullMutableValueModel<S>(), model));
-	}
-
-	private <S> TemplateComponentBindingBuilder<T, C> with(ValueModelDelegator<T, S> valueModelDelegator)
-	{
-		binder.bind(valueModelDelegator).to((HasValue<S>) component);
-		BindingSync.addCondition(valueModelDelegator);
-		return this;
-	}
-
-	public <S> TemplateComponentBindingBuilder<T, C> to(final ValueSource<S> valueSource)
-	{
-		ValueModelDelegator<T, S> valueModelDelegator= new ValueModelDelegator<T, S>(new NullMutableValueModel<S>()
-		{
-			public S getDelegatedValue()
-			{
-				return valueSource.getValue();
-			}
-		}, model);
-
-		return with(valueModelDelegator);
-	}
-
-	public C build()
-	{
-		panel.addChild(component);
-		return component;
-	}
-
-	public TemplateComponentBindingBuilder<T, C> onInput(InputListener inputListener)
-	{
-		component.addListener(InputListener.class, inputListener);
-		return this;
-	}
+    public TemplateComponentBindingBuilder<T, C> onInput(InputListener inputListener)
+    {
+	component.addListener(InputListener.class, inputListener);
+	return this;
+    }
 
 }
