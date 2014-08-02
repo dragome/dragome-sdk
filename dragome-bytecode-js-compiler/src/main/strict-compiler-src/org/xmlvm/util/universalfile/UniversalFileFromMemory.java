cab07abb93deab5749280117e399c7e65c8bd20c
Fernando Petrola <fernando.petrola@dragome.com>
2014-08-02 01:27:56 -0300

    license header fixed

--- dragome-bytecode-js-compiler/src/main/strict-compiler-src/org/xmlvm/util/universalfile/UniversalFileFromMemory.java
+++ dragome-bytecode-js-compiler/src/main/strict-compiler-src/org/xmlvm/util/universalfile/UniversalFileFromMemory.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2011-2014 Fernando Petrola
- * 
- *  This file is part of Dragome SDK.
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
 /* Copyright (c) 2002-2011 by XMLVM.org
  *
  * Project Info:  http://www.xmlvm.org
