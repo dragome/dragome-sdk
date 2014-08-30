a269f6df2a4163c733d7003c7a57f8f65375ac9a
Fernando Petrola <fernando.petrola@dragome.com>
2014-08-29 23:07:30 -0300

    Style synchronization support added Method for removing duplicated styles

--- dragome-core/src/main/java/com/dragome/render/html/components/DefaultStyleChangedListener.java
+++ dragome-core/src/main/java/com/dragome/render/html/components/DefaultStyleChangedListener.java
@@ -10,6 +10,10 @@
  ******************************************************************************/
 package com.dragome.render.html.components;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.w3c.dom.Element;
 
 import com.dragome.model.interfaces.Style;
@@ -29,8 +33,13 @@ public final class DefaultStyleChangedListener implements StyleChangedListener
 		Element element= ServiceLocator.getInstance().getDomHandler().getElementBySelector("[" + AbstractHTMLComponentRenderer.COMPONENT_ID_ATTRIBUTE + "=\"" + entityId + "\"]");
 		if (element != null)
 		{
-			if (style.getName() == null)
-				style.setName(element.getAttribute("class"));
+			if (!style.isSynchronized())
+			{
+				String result= (element.getAttribute("class") + (style.getName() != null ? " " + style.getName() : "")).trim();
+
+				style.setName(deDup(result));
+				style.setSynchronized(true);
+			}
 			else
 			{
 				if (style.getName().trim().length() > 0)
@@ -50,16 +59,34 @@ public final class DefaultStyleChangedListener implements StyleChangedListener
 		}
 	}
 
+	public String deDup(String s)
+	{
+		String[] strArr= s.split(" ");
+		Set<String> set= new LinkedHashSet<String>(Arrays.asList(strArr));
+		String[] result= new String[set.size()];
+		set.toArray(result);
+		StringBuilder res= new StringBuilder();
+		for (int i= 0; i < result.length; i++)
+		{
+			String string= result[i];
+			if (i == result.length - 1)
+				res.append(string);
+			else
+				res.append(string).append(" ");
+		}
+		return res.toString();
+	}
+
 	public void boundsChanged(Style style)
 	{
 		String entityId= DragomeEntityManager.getEntityId(style.getVisualComponent());
 		Element element= ServiceLocator.getInstance().getDomHandler().getElementBySelector("[" + AbstractHTMLComponentRenderer.COMPONENT_ID_ATTRIBUTE + "=\"" + entityId + "\"]");
 		if (element != null)
 		{
-		    String styleString= "position: relative; left: ${left}px;top: ${top}px;";
-		    styleString= styleString.replace("${left}", style.getBounds().getX()+"");
-		    styleString= styleString.replace("${top}", style.getBounds().getY()+"");
-		    element.setAttribute("style", styleString);
+			String styleString= "position: relative; left: ${left}px;top: ${top}px;";
+			styleString= styleString.replace("${left}", style.getBounds().getX() + "");
+			styleString= styleString.replace("${top}", style.getBounds().getY() + "");
+			element.setAttribute("style", styleString);
 		}
 	}
 }
