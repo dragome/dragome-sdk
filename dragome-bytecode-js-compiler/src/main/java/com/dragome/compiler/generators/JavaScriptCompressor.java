7955597541a61f3f8cbcc921190ac33ed369b509
Prototype <moritz.schleunes@gmail.com>
2014-11-28 22:57:24 +0100

    Just some small changes.
    Removed while(true) 
    
    and changed StringBuffer to StringBuilder since we dont need
    threadsafness at this point

--- dragome-bytecode-js-compiler/src/main/java/com/dragome/compiler/generators/JavaScriptCompressor.java
+++ dragome-bytecode-js-compiler/src/main/java/com/dragome/compiler/generators/JavaScriptCompressor.java
@@ -9,34 +9,32 @@ import java.io.InputStreamReader;
 public class JavaScriptCompressor
 {
 
-	private int CODE= 0;
+	private int CODE = 0;
 
-	private int LINECOMMENT= 1;
+	private int LINECOMMENT = 1;
 
-	private int MULTILINECOMMENT= 2;
+	private int MULTILINECOMMENT = 2;
 
-	private int STRING= 3;
+	private int STRING = 3;
 
 	public String compress(InputStream input) throws FileNotFoundException, IOException
 	{
-		BufferedReader reader= new BufferedReader(new InputStreamReader(input));
-		StringBuffer buffer= new StringBuffer();
-		int state= CODE;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+		StringBuilder buffer = new StringBuilder();
+		int state = CODE;
 		char c;
-		char lastC= 0;
+		char lastC = 0;
 
-		do
-		{
-			int i= reader.read();
-			if (i == -1)
-				break;
-			c= (char) i;
+		int i;
+
+		while ((i = reader.read()) != -1) {
+			c = (char) i;
 
 			if (state == LINECOMMENT)
 			{
 				if (c == '\n')
 				{
-					state= CODE;
+					state = CODE;
 					buffer.append(c);
 				}
 			}
@@ -44,35 +42,35 @@ public class JavaScriptCompressor
 			{
 				if (c == '/' && lastC == '*')
 				{
-					state= CODE;
+					state = CODE;
 				}
 			}
 			else if (state == STRING)
 			{
 				if (c == '"')
 				{
-					state= CODE;
+					state = CODE;
 				}
 				buffer.append(c);
 			}
 			else if (c == '/' && lastC == c)
 			{
-				state= LINECOMMENT;
+				state = LINECOMMENT;
 				buffer.deleteCharAt(buffer.length() - 1);
 			}
 			else if (c == '*' && lastC == '/')
 			{
-				state= MULTILINECOMMENT;
+				state = MULTILINECOMMENT;
 				buffer.deleteCharAt(buffer.length() - 1);
 			}
 			else if (c == '"')
 			{
-				state= STRING;
+				state = STRING;
 				buffer.append(c);
 			}
 			else
 			{
-				int length= buffer.length();
+				int length = buffer.length();
 				if (length > 0 && Character.isWhitespace(c) && Character.isWhitespace(buffer.charAt(length - 1)))
 				{
 
@@ -82,12 +80,12 @@ public class JavaScriptCompressor
 					buffer.append(c);
 				}
 			}
-			lastC= c;
+			lastC = c;
 		}
-		while (true);
 
 		reader.close();
 
+		buffer.trimToSize();
 		return buffer.toString();
 	}
 
