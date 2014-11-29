ce5d6bddd347665c9931521e2fa49454d774620e
Prototype1 <moritz.schleunes@gmail.com>
2014-11-29 11:57:28 +0100

    Just a few changes. Fixed some warning and made JavaScriptCompressor more read friendly

--- dragome-bytecode-js-compiler/src/main/java/com/dragome/compiler/generators/JavaScriptCompressor.java
+++ dragome-bytecode-js-compiler/src/main/java/com/dragome/compiler/generators/JavaScriptCompressor.java
@@ -9,19 +9,20 @@ import java.io.InputStreamReader;
 public class JavaScriptCompressor
 {
 
-	private int CODE = 0;
+	private final static int DEFAULT_CODE = 0;
 
-	private int LINECOMMENT = 1;
+	private final static int LINECOMMENT = 1;
 
-	private int MULTILINECOMMENT = 2;
+	private final static int MULTILINECOMMENT = 2;
 
-	private int STRING = 3;
+	private final static int STRING = 3;
 
 	public String compress(InputStream input) throws FileNotFoundException, IOException
 	{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-		StringBuilder buffer = new StringBuilder();
-		int state = CODE;
+		StringBuilder builder = new StringBuilder();
+		
+		int state = DEFAULT_CODE;
 		char c;
 		char lastC = 0;
 
@@ -34,50 +35,50 @@ public class JavaScriptCompressor
 			{
 				if (c == '\n')
 				{
-					state = CODE;
-					buffer.append(c);
+					state = DEFAULT_CODE;
+					builder.append(c);
 				}
 			}
 			else if (state == MULTILINECOMMENT)
 			{
 				if (c == '/' && lastC == '*')
 				{
-					state = CODE;
+					state = DEFAULT_CODE;
 				}
 			}
 			else if (state == STRING)
 			{
 				if (c == '"')
 				{
-					state = CODE;
+					state = DEFAULT_CODE;
 				}
-				buffer.append(c);
+				builder.append(c);
 			}
 			else if (c == '/' && lastC == c)
 			{
 				state = LINECOMMENT;
-				buffer.deleteCharAt(buffer.length() - 1);
+				builder.deleteCharAt(builder.length() - 1);
 			}
 			else if (c == '*' && lastC == '/')
 			{
 				state = MULTILINECOMMENT;
-				buffer.deleteCharAt(buffer.length() - 1);
+				builder.deleteCharAt(builder.length() - 1);
 			}
 			else if (c == '"')
 			{
 				state = STRING;
-				buffer.append(c);
+				builder.append(c);
 			}
 			else
 			{
-				int length = buffer.length();
-				if (length > 0 && Character.isWhitespace(c) && Character.isWhitespace(buffer.charAt(length - 1)))
+				int length = builder.length();
+				if (length > 0 && Character.isWhitespace(c) && Character.isWhitespace(builder.charAt(length - 1)))
 				{
 
 				}
 				else
 				{
-					buffer.append(c);
+					builder.append(c);
 				}
 			}
 			lastC = c;
@@ -85,8 +86,8 @@ public class JavaScriptCompressor
 
 		reader.close();
 
-		buffer.trimToSize();
-		return buffer.toString();
+		builder.trimToSize();
+		return builder.toString();
 	}
 
 }
