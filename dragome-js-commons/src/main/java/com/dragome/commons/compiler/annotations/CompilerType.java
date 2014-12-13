7aa19ad834a2062f5614efd98a403ddfa79dded4
Fernando Petrola <fernando.petrola@dragome.com>
2014-12-12 23:03:02 -0300

    Changing from GPLv3 to ASLv2 these modules: js-jre, method-logger, guia, form-bindings, callback-evictor, js-commons

--- dragome-js-commons/src/main/java/com/dragome/commons/compiler/annotations/CompilerType.java
+++ dragome-js-commons/src/main/java/com/dragome/commons/compiler/annotations/CompilerType.java
@@ -1,13 +1,18 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2011-2014 Fernando Petrola
- * 
- *  This file is part of Dragome SDK.
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.dragome.commons.compiler.annotations;
 
 public enum CompilerType
