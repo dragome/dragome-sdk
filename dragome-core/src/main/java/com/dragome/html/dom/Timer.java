c6432cbf08f9d2c18091c0f610164aaa52479e8d
Fernando Petrola <fernando.petrola@dragome.com>
2014-12-31 20:05:26 -0300

    Timer class improvements

--- dragome-core/src/main/java/com/dragome/html/dom/Timer.java
+++ dragome-core/src/main/java/com/dragome/html/dom/Timer.java
@@ -35,13 +35,6 @@ public class Timer
 
 	private void setupTimer(final Runnable runnable)
 	{
-		setupTimer(runnable, milliseconds);
-		ScriptHelper.put("milliseconds", milliseconds, this);
-		return ScriptHelper.eval("setInterval('window.wrappedCallback.$onSuccess___java_lang_Object$void(new String())', milliseconds)", this);
-	}
-
-	private void setupTimer(final Runnable runnable, int milliseconds)
-	{
 		AsyncCallback<String> asyncCallback= new AsyncCallback<String>()
 		{
 			public void onSuccess(String result)
@@ -58,14 +51,6 @@ public class Timer
 		ScriptHelper.put("wrappedCallback", wrappedCallback, this);
 		ScriptHelper.eval("window.wrappedCallback=wrappedCallback", this);
 	}
-	
-	public Object setTimeout(final Runnable runnable, int milliseconds)
-	{
-		setupTimer(runnable, milliseconds);
-		ScriptHelper.put("milliseconds", milliseconds, this);
-		return ScriptHelper.eval("setTimeout('window.wrappedCallback.$onSuccess___java_lang_Object$void(new String())', milliseconds)", this);
-	}
-	
 
 	public Timer setTimeout(final Runnable runnable, int milliseconds)
 	{
