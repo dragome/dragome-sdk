8597e278664208e47452c2dcf5031889f1d7d502
Fernando Petrola <fernando.petrola@dragome.com>
2014-12-31 20:05:06 -0300

    New utils method for creating temp dirs

--- dragome-core/src/main/java/com/dragome/helpers/Utils.java
+++ dragome-core/src/main/java/com/dragome/helpers/Utils.java
@@ -15,8 +15,12 @@
  */
 package com.dragome.helpers;
 
+import java.io.File;
+
 public class Utils
 {
+	public static final int TEMP_DIR_ATTEMPTS= 10000;
+
 	public static String convertDashedToCamel(String input)
 	{
 		StringBuilder result= new StringBuilder();
@@ -28,6 +32,34 @@ public class Utils
 			result.append(charToAdd);
 		}
 		return result.toString();
+	}
+
+	public static File createTempDir()
+	{
+		File baseDir= new File(System.getProperty("java.io.tmpdir"));
+		String baseName= System.currentTimeMillis() + "-";
+
+		for (int counter= 0; counter < TEMP_DIR_ATTEMPTS; counter++)
+		{
+			File tempDir= new File(baseDir, baseName + counter);
+			if (tempDir.mkdir())
+			{
+				return tempDir;
+			}
+		}
+		throw new IllegalStateException("Failed to create directory within " + TEMP_DIR_ATTEMPTS + " attempts (tried " + baseName + "0 to " + baseName + (TEMP_DIR_ATTEMPTS - 1) + ')');
+	}
+
+	public static File createTempDir(String name)
+	{
+		File baseDir= new File(System.getProperty("java.io.tmpdir"));
+
+		File tempDir= new File(baseDir, name);
+		if (tempDir.exists() || tempDir.mkdir())
+			return tempDir;
+		else
+			throw new IllegalStateException("Failed to create directory within " + baseDir + ": " + name);
+
 	}
 
 }
