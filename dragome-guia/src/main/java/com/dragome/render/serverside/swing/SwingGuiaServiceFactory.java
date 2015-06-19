da9fdc1df2c896a0aac0c334b2c985414170ece4
Fernando Petrola <fernando.petrola@dragome.com>
2015-06-19 16:22:53 -0300

    Temporal patch until Swing implementation goes to other module

--- dragome-guia/src/main/java/com/dragome/render/serverside/swing/SwingGuiaServiceFactory.java
+++ dragome-guia/src/main/java/com/dragome/render/serverside/swing/SwingGuiaServiceFactory.java
@@ -1,12 +1,14 @@
 package com.dragome.render.serverside.swing;
 
 import com.dragome.guia.GuiaServiceFactory;
+import com.dragome.guia.events.listeners.interfaces.StyleChangedListener;
+import com.dragome.model.interfaces.Style;
 import com.dragome.render.interfaces.TemplateHandler;
-import com.dragome.templates.interfaces.TemplateLoadingStrategy;
 import com.dragome.templates.interfaces.TemplateListener;
+import com.dragome.templates.interfaces.TemplateLoadingStrategy;
 import com.dragome.templates.interfaces.TemplateManager;
 
-public class SwingGuiaServiceFactory implements GuiaServiceFactory
+public class SwingGuiaServiceFactory 
 {
 	public TemplateManager createTemplateManager()
 	{
@@ -26,5 +28,19 @@ public class SwingGuiaServiceFactory implements GuiaServiceFactory
 	public TemplateListener getTemplateListener()
 	{
 		return new SwingTemplateListener();
+	}
+
+	public StyleChangedListener getStyleChangeListener()
+	{
+		return new StyleChangedListener()
+		{
+			public void styleChanged(Style style)
+			{
+			}
+			
+			public void boundsChanged(Style style)
+			{
+			}
+		};
 	}
 }
