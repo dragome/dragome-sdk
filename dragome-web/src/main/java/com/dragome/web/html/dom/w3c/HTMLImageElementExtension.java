cbc2e854d4a423e6abc9a2739dc43a598c062388
Fernando Petrola <fernando.petrola@dragome.com>
2016-03-22 02:21:35 -0300

    adding html elements extensions

--- dragome-web/src/main/java/com/dragome/web/html/dom/w3c/HTMLImageElementExtension.java
+++ dragome-web/src/main/java/com/dragome/web/html/dom/w3c/HTMLImageElementExtension.java
@@ -2,8 +2,12 @@ package com.dragome.web.html.dom.w3c;
 
 import org.w3c.dom.html.HTMLImageElement;
 
+import com.dragome.commons.DelegateCode;
+
 public interface HTMLImageElementExtension extends HTMLImageElement
 {
+	@DelegateCode(eval= "this.node.width")
 	public int getWidthAsInteger();
+	@DelegateCode(eval= "this.node.height")
 	public int getHeightAsInteger();
 }
