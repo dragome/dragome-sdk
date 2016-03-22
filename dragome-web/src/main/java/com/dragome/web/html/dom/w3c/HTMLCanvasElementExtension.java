2070aa79b335ac41c4996cbc4e70112d03a6eff6
Fernando Petrola <fernando.petrola@dragome.com>
2016-03-22 02:21:35 -0300

    adding html elements extensions

---
+++ dragome-web/src/main/java/com/dragome/web/html/dom/w3c/HTMLCanvasElementExtension.java
@@ -0,0 +1,9 @@
+package com.dragome.web.html.dom.w3c;
+
+import org.w3c.dom.html.HTMLCanvasElement;
+
+public interface HTMLCanvasElementExtension extends HTMLCanvasElement
+{
+	public void setCoordinateSpaceWidth(int width);
+	public void setCoordinateSpaceHeight(int height);
+}
