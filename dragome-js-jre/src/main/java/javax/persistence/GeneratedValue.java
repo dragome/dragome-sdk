9e394bf8614895edf806b02e7f0bbabf6d168075
Fernando Petrola <fernando.petrola@dragome.com>
2016-03-22 01:27:48 -0300

    adding persistence annotations for examples

---
+++ dragome-js-jre/src/main/java/javax/persistence/GeneratedValue.java
@@ -0,0 +1,17 @@
+package javax.persistence;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ METHOD, FIELD })
+@Retention(RUNTIME)
+
+public @interface GeneratedValue
+{
+	GenerationType strategy() default GenerationType.AUTO;
+	String generator() default "";
+}
---
+++ dragome-js-jre/src/main/java/javax/persistence/Id.java
@@ -0,0 +1,14 @@
+package javax.persistence;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD })
+@Retention(RUNTIME)
+public @interface Id
+{
+}
