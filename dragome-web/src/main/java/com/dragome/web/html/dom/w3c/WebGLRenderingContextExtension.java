d4eb28b059495f52ff0e810aa40a3641aa335694
Fernando Petrola <fernando.petrola@dragome.com>
2016-03-22 01:28:17 -0300

    adding html elements extensions

---
+++ dragome-web/src/main/java/com/dragome/web/html/dom/w3c/WebGLRenderingContextExtension.java
@@ -0,0 +1,20 @@
+package com.dragome.web.html.dom.w3c;
+
+import org.w3c.dom.webgl.WebGLRenderingContext;
+import org.w3c.dom.webgl.WebGLUniformLocation;
+
+public interface WebGLRenderingContextExtension extends WebGLRenderingContext
+{
+    public void uniform1iv(WebGLUniformLocation location, int[] v);
+	public void uniform1fv(WebGLUniformLocation loc, float[] v);
+	public void uniform2fv(WebGLUniformLocation loc, float[] v);
+	public void uniform2iv(WebGLUniformLocation loc, int[] v);
+	public void uniform3fv(WebGLUniformLocation loc, float[] v);
+	public void uniform3iv(WebGLUniformLocation loc, int[] v);
+	public void uniform4fv(WebGLUniformLocation loc, float[] v);
+	public void uniform4iv(WebGLUniformLocation loc, int[] v);
+	public void uniformMatrix2fv(WebGLUniformLocation loc, boolean transpose, float[] value);
+	public void uniformMatrix3fv(WebGLUniformLocation loc, boolean transpose, float[] value);
+	public void uniformMatrix4fv(WebGLUniformLocation loc, boolean transpose, float[] value);
+}
+
