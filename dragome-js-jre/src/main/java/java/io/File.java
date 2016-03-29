081ba5b36154f5051304755632d599c4cf4aa59d
Fernando Petrola <fernando.petrola@dragome.com>
2016-03-29 01:34:55 -0300

    fixing null pathname bug

--- dragome-js-jre/src/main/java/java/io/File.java
+++ dragome-js-jre/src/main/java/java/io/File.java
@@ -17,259 +17,319 @@ package java.io;
 
 import java.net.URI;
 
-public class File implements Serializable, Comparable<File> {
+public class File implements Serializable, Comparable<File>
+{
+	public static final char separatorChar= '/';
 
-    public static final char separatorChar = '/';
+	public static final String separator= "" + separatorChar;
 
-    public static final String separator = "" + separatorChar;
+	public static final char pathSeparatorChar= ':';
 
-    public static final char pathSeparatorChar = ':';
+	public static final String pathSeparator= "" + pathSeparatorChar;
 
-    public static final String pathSeparator = "" + pathSeparatorChar;
+	public static final File ROOT= new File("");
 
-    public static final File ROOT = new File("");
+	File parent;
+	String name;
 
-    File parent;
-    String name;
+	public File(String pathname)
+	{
+		if (pathname != null)
+		{
+			while (pathname.endsWith(separator) && pathname.length() > 0)
+			{
+				pathname= pathname.substring(0, pathname.length() - 1);
+			}
 
-    public File(String pathname) {
-        while (pathname.endsWith(separator) && pathname.length() > 0) {
-            pathname = pathname.substring(0, pathname.length() - 1);
-        }
+			int cut= pathname.lastIndexOf(separatorChar);
+			if (cut == -1)
+			{
+				name= pathname;
+			}
+			else if (cut == 0)
+			{
+				name= pathname.substring(cut);
+				parent= name.length() == 0 ? null : ROOT;
+			}
+			else
+			{
+				name= pathname.substring(cut + 1);
+				parent= new File(pathname.substring(0, cut));
+			}
+		}
+	}
 
-        int cut = pathname.lastIndexOf(separatorChar);
-        if (cut == -1) {
-            name = pathname;
-        } else if (cut == 0) {
-            name = pathname.substring(cut);
-            parent = name.length() == 0 ? null : ROOT;
-        } else {
-            name = pathname.substring(cut + 1);
-            parent = new File(pathname.substring(0, cut));
-        }
-    }
+	public File(String parent, String child)
+	{
+		this(new File(parent), child);
+	}
 
-    public File(String parent, String child) {
-        this(new File(parent), child);
-    }
+	public File(File parent, String child)
+	{
+		this.parent= parent;
+		this.name= child;
+	}
 
-    public File(File parent, String child) {
-        this.parent = parent;
-        this.name = child;
-    }
+	public File(URI uri)
+	{
+		// TODO Auto-generated constructor stub
+	}
 
-    public File(URI uri) {
-        // TODO Auto-generated constructor stub
-    }
+	public String getName()
+	{
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getParent()
+	{
+		return parent == null ? "" : parent.getPath();
+	}
 
-    public String getParent() {
-        return parent == null ? "" : parent.getPath();
-    }
+	public File getParentFile()
+	{
+		return parent;
+	}
 
-    public File getParentFile() {
-        return parent;
-    }
+	public String getPath()
+	{
+		return parent == null ? name : (parent.getPath() + separatorChar + name);
+	}
 
-    public String getPath() {
-        return parent == null ? name
-                : (parent.getPath() + separatorChar + name);
-    }
+	private boolean isRoot()
+	{
+		return name.length() == 0 && parent == null;
+	}
 
-    private boolean isRoot() {
-        return name.length() == 0 && parent == null;
-    }
+	public boolean isAbsolute()
+	{
+		if (isRoot())
+		{
+			return true;
+		}
+		if (parent == null)
+		{
+			return false;
+		}
+		return parent.isAbsolute();
+	}
 
-    public boolean isAbsolute() {
-        if (isRoot()) {
-            return true;
-        }
-        if (parent == null) {
-            return false;
-        }
-        return parent.isAbsolute();
-    }
+	public String getAbsolutePath()
+	{
+		String path= getAbsoluteFile().getPath();
+		return path.length() == 0 ? "/" : path;
+	}
 
-    public String getAbsolutePath() {
-        String path = getAbsoluteFile().getPath();
-        return path.length() == 0 ? "/" : path;
-    }
+	public File getAbsoluteFile()
+	{
+		if (isAbsolute())
+		{
+			return this;
+		}
+		if (parent == null)
+		{
+			return new File(ROOT, name);
+		}
+		return new File(parent.getAbsoluteFile(), name);
+	}
 
-    public File getAbsoluteFile() {
-        if (isAbsolute()) {
-            return this;
-        }
-        if (parent == null) {
-            return new File(ROOT, name);
-        }
-        return new File(parent.getAbsoluteFile(), name);
-    }
+	public String getCanonicalPath()
+	{
+		return getCanonicalFile().getAbsolutePath();
+	}
 
-    public String getCanonicalPath() {
-        return getCanonicalFile().getAbsolutePath();
-    }
+	public File getCanonicalFile()
+	{
+		File cParent= parent == null ? null : parent.getCanonicalFile();
+		if (name.equals("."))
+		{
+			return cParent == null ? ROOT : cParent;
+		}
+		if (cParent != null && cParent.name.length() == 0)
+		{
+			cParent= null;
+		}
+		if (name.equals(".."))
+		{
+			if (cParent == null)
+			{
+				return ROOT;
+			}
+			if (cParent.parent == null)
+			{
+				return ROOT;
+			}
+			return cParent.parent;
+		}
+		if (cParent == null && name.length() > 0)
+		{
+			return new File(ROOT, name);
+		}
+		return new File(cParent, name);
+	}
 
-    public File getCanonicalFile() {
-        File cParent = parent == null ? null : parent.getCanonicalFile();
-        if (name.equals(".")) {
-            return cParent == null ? ROOT : cParent;
-        }
-        if (cParent != null && cParent.name.length() == 0) {
-            cParent = null;
-        }
-        if (name.equals("..")) {
-            if (cParent == null) {
-                return ROOT;
-            }
-            if (cParent.parent == null) {
-                return ROOT;
-            }
-            return cParent.parent;
-        }
-        if (cParent == null && name.length() > 0) {
-            return new File(ROOT, name);
-        }
-        return new File(cParent, name);
-    }
+	public boolean canRead()
+	{
+		return true;
+	}
 
-    public boolean canRead() {
-        return true;
-    }
+	public boolean canWrite()
+	{
+		return true;
+	}
 
-    public boolean canWrite() {
-        return true;
-    }
+	public boolean exists()
+	{
+		return true;
+	}
 
-    public boolean exists() {
-        return true;
-    }
+	public boolean isDirectory()
+	{
+		return false;
+	}
 
-    public boolean isDirectory() {
-        return false;
-    }
+	public boolean isFile()
+	{
 
-    public boolean isFile() {
+		return false;
 
-        return false;
+	}
 
-    }
+	public boolean isHidden()
+	{
+		return false;
+	}
 
-    public boolean isHidden() {
-        return false;
-    }
+	public long lastModified()
+	{
+		return 0;
+	}
 
-    public long lastModified() {
-        return 0;
-    }
+	public long length()
+	{
 
-    public long length() {
+		return 0;
 
-        return 0;
+	}
 
-    }
+	public boolean createNewFile() throws IOException
+	{
+		if (exists())
+		{
+			return false;
+		}
 
-    public boolean createNewFile() throws IOException {
-        if (exists()) {
-            return false;
-        }
+		return parent.exists();
+	}
 
-        return parent.exists();
-    }
+	public boolean delete()
+	{
 
-    public boolean delete() {
+		return exists();
 
-        return exists();
+	}
 
-    }
+	public void deleteOnExit()
+	{
+		throw new RuntimeException("not implemented");
+	}
 
-    public void deleteOnExit() {
-        throw new RuntimeException("not implemented");
-    }
+	public String[] list()
+	{
+		throw new RuntimeException("not implemented");
+	}
 
-    public String[] list() {
-        throw new RuntimeException("not implemented");
-    }
+	public File[] listFiles()
+	{
+		return listFiles(null);
+	}
 
-    public File[] listFiles() {
-        return listFiles(null);
-    }
+	public File[] listFiles(FilenameFilter filter)
+	{
 
-    public File[] listFiles(FilenameFilter filter) {
+		return null;
+	}
 
-        return null;
-    }
+	public boolean mkdir()
+	{
 
-    public boolean mkdir() {
+		if (parent != null && !parent.exists())
+		{
+			return false;
+		}
 
-        if (parent != null && !parent.exists()) {
-            return false;
-        }
+		return !exists();
 
-        return !exists();
+	}
 
-    }
+	public boolean mkdirs()
+	{
+		if (parent != null)
+		{
+			parent.mkdirs();
+		}
+		return mkdir();
+	}
 
-    public boolean mkdirs() {
-        if (parent != null) {
-            parent.mkdirs();
-        }
-        return mkdir();
-    }
+	public boolean renameTo(File dest)
+	{
+		throw new RuntimeException("renameTo()");
+	}
 
-    public boolean renameTo(File dest) {
-        throw new RuntimeException("renameTo()");
-    }
+	public boolean setLastModified(long time)
+	{
+		return false;
+	}
 
-    public boolean setLastModified(long time) {
-        return false;
-    }
+	public boolean setReadOnly()
+	{
+		return false;
+	}
 
-    public boolean setReadOnly() {
-        return false;
-    }
+	public static File[] listRoots()
+	{
+		return new File[] { ROOT };
+	}
 
-    public static File[] listRoots() {
-        return new File[]{ROOT};
-    }
+	public static File createTempFile(String prefix, String suffix, File directory) throws IOException
+	{
+		throw new RuntimeException("not implemented");
+	}
 
-    public static File createTempFile(String prefix, String suffix,
-            File directory) throws IOException {
-        throw new RuntimeException("not implemented");
-    }
+	public static File createTempFile(String prefix, String suffix) throws IOException
+	{
+		throw new RuntimeException("not implemented");
+	}
 
-    public static File createTempFile(String prefix, String suffix)
-            throws IOException {
-        throw new RuntimeException("not implemented");
-    }
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (!(obj instanceof File))
+		{
+			return false;
+		}
+		return getPath().equals(((File) obj).getPath());
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof File)) {
-            return false;
-        }
-        return getPath().equals(((File) obj).getPath());
-    }
+	@Override
+	public int hashCode()
+	{
+		return parent != null ? parent.hashCode() + name.hashCode() : name.hashCode();
+	}
 
-    @Override
-    public int hashCode() {
-        return parent != null ? parent.hashCode() + name.hashCode() : name
-                .hashCode();
-    }
+	@Override
+	public String toString()
+	{
+		return name;
+	}
 
-    @Override
-    public String toString() {
-        return name;
-    }
+	public int compareTo(File another)
+	{
+		return 0;
+	}
 
-    public int compareTo(File another) {
-        return 0;
-    }
-
-    public Object toURI() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	public Object toURI()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
