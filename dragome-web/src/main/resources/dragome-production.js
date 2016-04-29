551497bc816250a8f0ed0196c5375c8ed8dfb04e
Fernando Petrola <fernando.petrola@dragome.com>
2016-04-29 11:17:23 -0300

    performance: using script tag for production js instead of eval

--- dragome-web/src/main/resources/dragome-production.js
+++ dragome-web/src/main/resources/dragome-production.js
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2011-2014 Fernando Petrola
- *
+ * 
  * This file is part of Dragome SDK.
- *
+ * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the GNU Public License v3.0 which accompanies
  * this distribution, and is available at http://www.gnu.org/licenses/gpl.html
@@ -73,7 +73,10 @@ function Uint8ToString(u8a) {
 				ajax.open("GET", "compiled-js/webapp-1.js", true);
 				ajax.responseType = "arraybuffer";
 				ajax.onload = function() {
-					eval(Uint8ToString(deflate.decompress(new Uint8Array(ajax.response))));
+					var code= Uint8ToString(deflate.decompress(new Uint8Array(ajax.response)));
+					var newScriptTag = document.createElement('script');
+					newScriptTag.appendChild(document.createTextNode(code));
+					document.body.appendChild(newScriptTag);
 				};
 				ajax.send();
 			});
