275c1f1066f65841a421fccf27b3a212b13dc945
Fernando Petrola <fernando.petrola@dragome.com>
2016-07-04 16:39:37 -0300

    adding length variable in js back in ArrayBufferFactory

--- dragome-web/src/main/java/com/dragome/web/html/dom/w3c/ArrayBufferFactory.java
+++ dragome-web/src/main/java/com/dragome/web/html/dom/w3c/ArrayBufferFactory.java
@@ -10,6 +10,7 @@ public class ArrayBufferFactory
 	@DelegateCode(ignore= true)
 	public static ArrayBuffer createArrayBuffer(int length)
 	{
+		ScriptHelper.put("length", length, null);
 		ArrayBuffer node= ScriptHelper.evalCasting("new ArrayBuffer(length)", ArrayBuffer.class, null);
 		return node;
 	}
