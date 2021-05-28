da5cba5ac93a6bc2b77efb9dc2f1da0e856f41cd
Fpetrola <fpetrola@ip-192-168-43-168.sa-east-1.compute.internal>
2021-05-28 18:49:21 -0300

    finding method before actual invocation

--- dragome-js-jre/src/main/java/java/lang/invoke/MethodHandle.java
+++ dragome-js-jre/src/main/java/java/lang/invoke/MethodHandle.java
@@ -21,12 +21,14 @@ import java.lang.reflect.TypeVariable;
 import java.util.Arrays;
 import java.util.List;
 
+import com.dragome.commons.ProxyRelatedInvocationHandler;
 import com.dragome.commons.javascript.ScriptHelper;
 
 public class MethodHandle
 {
 	private Object x;
 	private Lookup lookup;
+	private Method foundMethod;
 
 	public MethodHandle(Lookup lookup)
 	{
@@ -35,42 +37,52 @@ public class MethodHandle
 
 	public MethodHandle bindTo(Object x)
 	{
+		if (x instanceof ProxyRelatedInvocationHandler)
+		{
+			ProxyRelatedInvocationHandler proxyRelatedInvocationHandler= (ProxyRelatedInvocationHandler) x;
+			x= proxyRelatedInvocationHandler.getProxy();
+		}
 		this.x= x;
+		findMethod();
 		return this;
 	}
 
 	public Object invokeWithArguments(Object... args) throws Throwable
 	{
+		ScriptHelper.put("type", foundMethod.getDeclaringClass(), this);
+		ScriptHelper.put("args", args, this);
+		ScriptHelper.put("proxy", x, this);
+		ScriptHelper.put("method", lookup.getMethod(), this);
+
+		boolean notMembers= ScriptHelper.evalBoolean("type.$$$nativeClass___java_lang_Object.$$members === undefined", this);
+		Object o;
+		if (notMembers)
+			o= ScriptHelper.eval("type.$$$nativeClass___java_lang_Object.prototype[method.$$$signature___java_lang_String].apply(proxy, args)", this);
+		else
+			o= ScriptHelper.eval("type.$$$nativeClass___java_lang_Object.$$members[method.$$$signature___java_lang_String].apply(proxy, args)", this);
+		return o;
+	}
+
+	public void findMethod()
+	{
 		Method method= lookup.getMethod();
 		Class<?> specialCaller= lookup.getSpecialCaller();
 		List<Method> internalGetMethods= specialCaller.internalGetMethods(false);
 
-		for (Method foundMethod : internalGetMethods)
+		for (Method aMethod : internalGetMethods)
 		{
-			if (foundMethod.getName().equals(method.getName()))
+			if (aMethod.getName().equals(method.getName()))
 			{
-				if (foundMethod.getReturnType().equals(method.getReturnType()))
+				if (aMethod.getReturnType().equals(method.getReturnType()))
 				{
 					List<TypeVariable<?>> typeParameters1= Arrays.asList(method.getTypeParameters());
-					List<TypeVariable<?>> typeParameters2= Arrays.asList(foundMethod.getTypeParameters());
+					List<TypeVariable<?>> typeParameters2= Arrays.asList(aMethod.getTypeParameters());
 					if (typeParameters1.equals(typeParameters2))
 					{
-						ScriptHelper.put("type", foundMethod.getDeclaringClass(), this);
-						ScriptHelper.put("args", args, this);
-						ScriptHelper.put("proxy", x, this);
-						ScriptHelper.put("method", method, this);
-
-						boolean notMembers= ScriptHelper.evalBoolean("type.$$$nativeClass___java_lang_Object.$$members === undefined", this);
-						Object o;
-						if (notMembers)
-							o= ScriptHelper.eval("type.$$$nativeClass___java_lang_Object.prototype[method.$$$signature___java_lang_String].apply(proxy, args)", this);
-						else
-							o= ScriptHelper.eval("type.$$$nativeClass___java_lang_Object.$$members[method.$$$signature___java_lang_String].apply(proxy, args)", this);
-						return o;
+						foundMethod= aMethod;
 					}
 				}
 			}
 		}
-		return null;
 	}
 }
